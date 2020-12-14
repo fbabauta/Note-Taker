57d8ab969ec9d2498121d90a297bf06c0420bfd7
francine babauta <francine.babauta@outlook.com>
2020-12-14 01:38:10 -0800

    added routes to api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,57 @@
+// require dependencies
+const fs = require("fs");
+const path = require("path");
+
+const jsonData = require("../db/db");
+
+module.exports = function (app) {
+    
+    // GET - get api data
+    app.get("/api/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../db/db.json"));
+    });
+
+    // POST - post new entry to api
+    app.post("/api/notes", function (req, res) {
+
+        // push new entry into jsonData array
+        let newNote = req.body;
+        jsonData.push(newNote);
+        renderDataID(jsonData);
+
+        // write a new file using the updated jsonData array
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(jsonData, null, 2), (err) => {
+            if (err) throw err
+        });
+
+        res.json(newNote);
+    });
+
+    // DELETE - delete entry from API
+    app.delete("/api/notes/:id", function (req, res) {
+        let deleteNoteID = req.params.id;
+
+        // delete entry who's id matches the requested id
+        for (var i = 0; i < jsonData.length; i++) {
+            if (deleteNoteID == jsonData[i].id) {
+                jsonData.splice(i, 1);
+            }
+        };
+
+        renderDataID(jsonData);
+
+        // write new file using the updated jsonData array
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(jsonData, null, 2), (err) => {
+            if (err) throw err
+        });
+
+        res.json(deleteNoteID);
+    });
+};
+
+// renders id property of the objects in the jsonData array
+function renderDataID(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].id = i + 1;
+    }
+};
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function (app) {
+    
+    // notes route
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    // home route 
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+
+};
