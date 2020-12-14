bdb26b8417adb1e6e4b0987c571eddc60ccebe57
francine babauta <francine.babauta@outlook.com>
2020-12-14 01:44:34 -0800

    changes to public folder

---
+++ public/assets/js/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Notepad</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/flatly/bootstrap.min.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&family=Baloo+Bhaina+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" type="image/png" href="/assets/images/favicon-32x32.png">
+  <link rel="stylesheet" href="/assets/css/styles.css"></link>
+  
